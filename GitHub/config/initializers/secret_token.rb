05eaa3ace3497f7f32cd5e772ac74e1d0010f315
Sandy Ramirez <sandyramirezp@gmail.com>
2012-04-26 11:06:27 -0400

    Create a new Proyect
    ~Rails new "GitHub"

---
+++ GitHub/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitHub::Application.config.secret_token = '9ef7cf43ca8d4399e33853a8cc1b2b98f0531a78e1df1d3020f089f5f12494af02cdc62b2955aa3d84b0fba61a1664fbbe9ec53fb4da05194146018cc6ea7cef'
---
+++ GitHub/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GitHub::Application.config.session_store :cookie_store, key: '_GitHub_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GitHub::Application.config.session_store :active_record_store
